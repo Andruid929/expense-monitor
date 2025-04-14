3e3fa144bf6e62ec43161a2badbb0532fddd573e
Andruid929 <princeajsa@gmail.com>
2025-04-14 20:14:45 +0200

    Created dedicated method for launching expense monitor in Main class

--- src/main/java/net/druidlabs/Main.java
+++ src/main/java/net/druidlabs/Main.java
@@ -1,6 +1,8 @@
 package net.druidlabs;
 
 import net.druidlabs.expensemonitor.cmdln.CommandListener;
+import net.druidlabs.expensemonitor.expenses.Expense;
+import net.druidlabs.expensemonitor.values.Strings;
 
 import java.util.Scanner;
 
@@ -9,6 +11,13 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        int amount = 9000;
+        String desc = Strings.TEST_DESC;
+
+        Expense expense = new Expense(amount, desc);
+    }
+
+    private static void runExpenseMonitor() { //Call in main method to start
         System.out.println("Expense tracker");
         System.out.println("Enter a command to get started or enter 'HELP' or 'H' for a list of commands");
 
